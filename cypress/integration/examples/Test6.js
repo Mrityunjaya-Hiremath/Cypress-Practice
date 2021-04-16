62b41f334aed36fc34e291698face656d3cfa48d
MJ Hiremath <mjhiremath@MJs-MacBook-Pro.local>
2021-04-16 09:37:17 +0530

    initial push

--- cypress/integration/examples/Test6.js
+++ cypress/integration/examples/Test6.js
@@ -4,6 +4,7 @@ describe('My test Suite', function () {
     it('My test cases', function () {
 
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/#/")
+        //Click on the hidden elements
         //cy.get('.mouse-hover-content').invoke('show')
         cy.contains('Top').click({force:true})
         cy.url().should('include','top')
